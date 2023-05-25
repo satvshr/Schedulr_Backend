74fd8ab2d843a26cd4ea0cd5118f170645c971d2
satvshr <112589278+satvshr@users.noreply.github.com>
2023-05-25 22:40:02 +0530

    encountering an error which tells me that savedEvents is not a function

--- backend/urls.py
+++ backend/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.auth, name="auth"),
     path("api", views.api, name="api"),
     path("get", views.get_events, name="get_events"),
+    path("deletion", views.deletion, name="deletion"),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,10 @@
 from django.conf import settings
 from google.oauth2 import service_account
 from google.auth.transport import requests
-from django.shortcuts import redirect
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, JsonResponse
 from googleapiclient.discovery import build
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from django.views.decorators.csrf import csrf_exempt
 import json
 
@@ -26,17 +26,14 @@ def auth(request):
     # Store the access token in the session
     request.session['access_token'] = access_token
 
-    return redirect('api')
+    return redirect('get')
 
 @csrf_exempt
 def api(request):
-    while request.method == 'POST':
+    if request.method == 'POST':
         service = build("calendar", "v3", credentials=credentials)
         event_data = json.loads(request.body.decode('utf-8'))
-        print(event_data)
-        # Get the list of events from the payload
         event_list = event_data if isinstance(event_data, list) else []
-        print(event_list)
         calendar_id = settings.GOOGLE_CALENDAR_ID
 
         for event in event_list:
@@ -46,48 +43,51 @@ def api(request):
             start_time = datetime.fromtimestamp(event.get("day", 0) / 1000.0)
             end_time = start_time + timedelta(days=1)
             event_color_id = event.get("colorId")
-            print(event_summary)
-            print(event_description)
-            print(start_time)
-            print(end_time)
-            print(event_color_id)
-            if event_id:
-                # Update an event
-                service.events().update(
-                    calendarId=calendar_id,
-                    eventId=event_id,
-                    body={
-                        "summary": event_summary,
-                        "start": {
-                            "dateTime": start_time.isoformat()
-                        },
-                        "end": {
-                            "dateTime": end_time.isoformat()
-                        },
-                        "colorId": event_color_id
-                    }
-                ).execute()
-            else:
                 # Create a new event
-                service.events().insert(
-                    calendarId=calendar_id,
-                    body={
-                        "summary": event_summary,
-                        "start": {
-                            "dateTime": start_time.isoformat(),
-                            "timeZone": "Asia/Kolkata" 
-                        },
-                        "end": {
-                            "dateTime": end_time.isoformat(),
-                            "timeZone": "Asia/Kolkata" 
+            service.events().insert(
+                calendarId=calendar_id,
+                body={
+                    "summary": event_summary,
+                    "description": event_description,
+                    "start": {
+                        "dateTime": start_time.isoformat(),
+                        "timeZone": "Asia/Kolkata"
+                    },
+                    "end": {
+                        "dateTime": end_time.isoformat(),
+                        "timeZone": "Asia/Kolkata"
+                    },
+                    "colorId": event_color_id
+                }
+            ).execute()
 
-                        },
-                        "colorId": event_color_id
-                    }
-                ).execute()
+        return HttpResponse('Event update/creation/deletion completed successfully!', headers={'Access-Control-Allow-Origin': 'http://localhost:3000'})
 
-        return HttpResponse('Event update/creation completed successfully!', headers={'Access-Control-Allow-Origin': 'http://localhost:3000'})
+@csrf_exempt
+def deletion(request):
+    if request.method == 'POST':
+        service = build("calendar", "v3", credentials=credentials)
+        event_data = json.loads(request.body.decode('utf-8'))
+        print(event_data)
+        event_list = event_data if isinstance(event_data, list) else []
+        calendar_id = settings.GOOGLE_CALENDAR_ID
 
+        for event in event_list:
+            event_summary = event.get("title")
+            start_time = datetime.fromtimestamp(event.get("day", 0) / 1000.0)
+
+            events = service.events().list(calendarId=calendar_id, q=event_summary).execute()
+            print(events)
+            items = events.get('items', [])
+            print(items)
+            print("HEL")
+            for it in items:
+                # if it['start'].get('dateTime') == start_time.isoformat():
+                    event_id = it['id']
+                    service.events().delete(calendarId=calendar_id, eventId=event_id).execute()
+                    print("HELLLOOOO")
+                
+        return HttpResponse("Deleted!")
 
 @csrf_exempt
 def get_events(request):
@@ -102,11 +102,31 @@ def get_events(request):
 
     # Process the list of events
     event_list = events.get('items', [])
+    event_data = []
+    color_map = {
+        "1" : "green",
+        "2": "blue",
+        "11" : "red"
+    }
     for event in event_list:
-        summary = event.get('summary', 'No summary')
-        start_time = event.get('start', {}).get('dateTime', 'No start time')
-        # end_time = event.get('end', {}).get('dateTime', 'No end time')
-        print(f"Summary: {summary}")
-        print(f"Start Time: {start_time}")
-        print("-----------------------")
-    return HttpResponse("okie")
+        date_time_string = event.get('start').get('dateTime')
+        date_time_obj = datetime.fromisoformat(date_time_string.replace("Z", "+00:00"))
+        utc_time = date_time_obj.astimezone(timezone.utc)
+        milliseconds = int(utc_time.timestamp() * 1000)
+        color_id = event.get('colorId')
+        data = {
+            'title' : event.get('summary'),
+            'description' : event.get('description'),
+            'colorId' : color_id,
+            'day': milliseconds,
+            'id' : event.get('id'),
+            'label' : color_map.get(color_id)
+        }
+        event_data.append(data)
+    
+    return JsonResponse(event_data, safe=False)
+
+            # event_id = event['id']
+            # event_summary = event['summary']
+            # event_description = event.get('description', '')
+            # event_color_id = event.get('colorId', '')
